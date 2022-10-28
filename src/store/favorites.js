3d6069df177a0452a7ce7504699c31dea9037ba7
carlosmab <carlosm.araujob@gmail.com>
2022-10-28 11:07:03 -0500

    pinia ready

--- src/store/favorites.js
+++ src/store/favorites.js
@@ -3,11 +3,17 @@ import { defineStore } from 'pinia';
 
 export const useFavoritesStore = defineStore("favorites", () => {
   const favorites = ref([]);
+
+  if (localStorage.getItem("favorites")) {
+    favorites.value = JSON.parse(localStorage.getItem("favorites"));
+  }
   const add = (poke) => {
     favorites.value.push(poke);
+    localStorage.setItem("favorites", JSON.stringify(favorites.value));
   }
   const remove = (id) => {
     favorites.value = favorites.value.filter((item) => item.id != id);
+    localStorage.setItem("favorites", JSON.stringify(favorites.value));
   }
   const find = (name) => {
     return favorites.value.find((item) => item.name == name);
